630d888dc8c835c38e1a214fedaaeba2f3cb92a0
Chris Banta <chrisbanta1@gmail.com>
2020-03-17 18:31:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -44,7 +44,7 @@ def total_gross(source)
 total_gross_of_earnings = 0
 director_index = 0
 
-while director_index , source.length do
+while director_index < source.length do
   total_gross_of_earnings += gross_for_director(source[director_index])
   director_index += 1
 end
