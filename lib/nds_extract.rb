b4a94a75e57a21f4923e1e7ddbd55d72aefe28ac
Chris Banta <chrisbanta1@gmail.com>
2020-03-17 18:38:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -41,14 +41,14 @@ end
 
 
 def total_gross(source)
-total_gross = 0
+total_gross_of_earnings = 0
 director_index = 0
 
 while director_index < source.length do
-  total_gross += gross_for_director(source[director_index])
+  total_gross_of_earnings += gross_for_director(source[director_index])
   director_index += 1
 end
-total_gross
+total_gross_of_earnings
 end
 
   # Write this implementation
